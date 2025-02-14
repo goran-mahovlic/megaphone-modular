faf2759fa633043f267be553bd9f3abb708b7c35
Paul Gardner-Stephen <Paul Gardner-Stephen>
2025-02-14 13:20:37 +1030

    procedurally generate pads and component box

--- tools/footprint-gen.c
+++ tools/footprint-gen.c
@@ -39,6 +39,140 @@ int main(int argc, char **argv)
   
   fprintf(stderr,"INFO: Module size = %fx%f mm, %d pins each side.\n",
 	  module_width, module_height, half_pin_count);
+
+  printf("(footprint \"MegaCastle2x%d-Module-Mfoo\"\n"
+	   "        (version 20240108)\n"
+	   "	    (generator \"pcbnew\")\n"
+	   "        (generator_version \"8.0\")\n"
+	   "        (layer \"F.Cu\")\n"
+	   "        (property \"Reference\" \"REF**\"\n"
+	   "                (at 0 11.43 0)\n"   // XXX - Set position of REF**
+	   "                (unlocked yes)\n"
+	   "	 (layer \"F.SilkS\")\n"
+	   "                (uuid \"08d1cbd6-6dbd-4696-9477-43e1380128be\")\n"
+	   "                (effects\n"
+	   "                        (font\n"
+	   "                                (size 1 1)\n"
+	   "                                (thickness 0.1)\n"
+	   "                        )\n"
+	   "                )\n"
+	   "        )\n",
+	   half_pin_count
+	   );
+
+	 printf("        (property \"Value\" \"MegaCastle2x%d-Module-Mfoo\"\n"
+		"                (at 0 12.47 0)\n"
+		"                (unlocked yes)\n"
+		"                (layer \"F.Fab\")\n"
+		"                (uuid \"7848b12f-b698-40ff-9b18-5d3dc1ad63e0\")\n"
+		"                (effects\n"
+		"                        (font\n"
+		"                                (size 1 1)\n"
+		"                                (thickness 0.15)\n"
+		"                        )\n"
+		"                )\n"
+		"        )\n",
+		half_pin_count);
+
+	 printf("        (property \"Footprint\" \"\"\n"
+		"                (at 0 0 0)\n"
+		"                (layer \"F.Fab\")\n"
+		"                (hide yes)\n"
+		"                (uuid \"24d79921-7c8a-4bf5-b40a-0bc77c603d9c\")\n"
+		"                (effects\n"
+		"                        (font\n"
+		"                                (size 1.27 1.27)\n"
+		"                                (thickness 0.15)\n"
+		"                        )\n"
+		"                )\n"
+		"        )\n"
+		);
+
+	 printf("        (property \"Datasheet\" \"\"\n"
+		"                (at 0 0 0)\n"
+		"                (layer \"F.Fab\")\n"
+		"                (hide yes)\n"
+		"                (uuid \"1a070192-ad50-44fd-8acb-aa45a22d9533\")\n"
+		"                (effects\n"
+		"                        (font\n"
+		"                                (size 1.27 1.27)\n"
+		"                                (thickness 0.15)\n"
+		"                        )\n"
+		"                )\n"
+		"       )\n"
+		);
+
+	 printf("	 (property \"Description\" \"\"\n"
+		"                (at 0 0 0)\n"
+		"                (layer \"F.Fab\")\n"
+		"                (hide yes)\n"
+		"                (uuid \"d6dd6c62-1d04-4d09-a07a-575c94e93110\")\n"
+		"                (effects\n"
+		"                        (font\n"
+		"                                (size 1.27 1.27)\n"
+		"                                (thickness 0.15)\n"
+		"                        )\n"
+		"                )\n"
+		"        )\n"
+		"        (attr smd)\n"
+		);
+
+	 // Draw rectangle for component area on rear for cut-out
+	 printf("         (fp_rect\n"
+		"                (start %.2f %.2f)\n"
+		"                (end %.2f %.2f)\n"
+		"                (stroke\n"
+		"                        (width 0.05)\n"
+		"                        (type default)\n"
+		"                )\n"
+		"                (fill none)\n"
+		"                (layer \"B.SilkS\")\n"
+		"                (uuid \"a267cc7c-3519-453e-849e-e978b46e7c98\")\n"
+		"	   )\n",
+		-co_width/2,co_height/2,
+		co_width/2,-co_height/2
+		);
+
+	 for(int i=0;i<half_pin_count;i++)
+	   {
+	     printf("       (pad \"%d\" thru_hole rect\n"
+		    "                (at %f %f)\n"
+		    "                (size 2.54 2)\n"
+		    "                (drill oval 2 1.5)\n"
+		    "                (property pad_prop_castellated)\n"
+		    "                (layers \"*.Cu\" \"*.Mask\")\n"
+		    "                (remove_unused_layers no)\n"
+		    "                (thermal_bridge_angle 45)\n"
+		    "                (uuid \"f85e55df-ccf1-4a29-ab8b-2b081b1da284\")\n"
+		    "        )\n",
+		    i+1,
+		    -module_width/2,
+		    -module_height/2 + 2.54/2.0 + 2.54*i
+		    );
+	   }
+
+	 for(int i=0;i<half_pin_count;i++)
+	   {
+	     printf("       (pad \"%d\" thru_hole rect\n"
+		    "                (at %f %f)\n"
+		    "                (size 2.54 2)\n"
+		    "                (drill oval 2 1.5)\n"
+		    "                (property pad_prop_castellated)\n"
+		    "                (layers \"*.Cu\" \"*.Mask\")\n"
+		    "                (remove_unused_layers no)\n"
+		    "                (thermal_bridge_angle 45)\n"
+		    "                (uuid \"f85e55df-ccf1-4a29-ab8b-2b081b1da284\")\n"
+		    "        )\n",
+		    i+1+half_pin_count,
+		    module_width/2,
+		    -module_height/2 + 2.54/2.0 + 2.54*i
+		    );
+	   }
+	 
+	 
+
+	 
+	 printf(")\n");
   
   return 0;
 }
