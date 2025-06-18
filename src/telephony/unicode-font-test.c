29f209f22bd357dce79a40f6e60deadd792f7a2b
Paul Gardner-Stephen <paul@m-e-g-a.org>
2025-06-18 19:50:25 +0930

    begin work on UTF-8 parsing

--- src/telephony/unicode-font-test.c
+++ src/telephony/unicode-font-test.c
@@ -291,6 +291,56 @@ char draw_glyph(int x, int y, int font, unsigned long codepoint,unsigned char co
   return 1;
 }
 
+unsigned long utf8_next_codepoint(unsigned char **s)
+{
+  unsigned char *p;
+  unsigned long cp;
+
+  if (!s || !(*s)) return 0L;
+
+  p = *s;
+  
+  if (p[0] < 0x80) {
+    cp = p[0];
+    (*s)++;
+    return cp;
+  }
+
+  // 2-byte sequence: 110xxxxx 10xxxxxx
+  if ((p[0] & 0xE0) == 0xC0) {
+    if ((p[1] & 0xC0) != 0x80) return 0xFFFD; // invalid continuation
+    cp = ((p[0] & 0x1F) << 6) | (p[1] & 0x3F);
+    *s += 2;
+    return cp;
+  }
+
+  // 3-byte sequence: 1110xxxx 10xxxxxx 10xxxxxx
+  if ((p[0] & 0xF0) == 0xE0) {
+    if ((p[1] & 0xC0) != 0x80 || (p[2] & 0xC0) != 0x80) return 0xFFFD;
+    cp = ((p[0] & 0x0F) << 12) |
+         ((p[1] & 0x3F) << 6) |
+         (p[2] & 0x3F);
+    *s += 3;
+    return cp;
+  }
+
+  // 4-byte sequence: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+  if ((p[0] & 0xF8) == 0xF0) {
+    if ((p[1] & 0xC0) != 0x80 || (p[2] & 0xC0) != 0x80 || (p[3] & 0xC0) != 0x80)
+      return 0xFFFD;
+    cp = ((p[0] & 0x07) << 18) |
+         ((p[1] & 0x3F) << 12) |
+         ((p[2] & 0x3F) << 6) |
+         (p[3] & 0x3F);
+    *s += 4;
+    return cp;
+  }
+
+  // Invalid or unsupported UTF-8 byte
+  (*s)++;
+  return 0xFFFD;
+}
+
 void main(void)
 {
   shared_resource_dir d;
