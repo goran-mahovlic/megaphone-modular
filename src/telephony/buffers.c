deea170da0ed6ce9a23c966ec8b5748be3fef4da
Paul Gardner-Stephen <paul@m-e-g-a.org>
2025-08-03 05:26:27 +0930

    more work on factoring out large buffers into a union

--- src/telephony/buffers.c
+++ src/telephony/buffers.c
@@ -1,4 +1,19 @@
 #include "includes.h"
+#include "records.h"
 #include "buffers.h"
 
 struct shared_buffers buffers;
+
+unsigned char buffers_lock(unsigned char module)
+{
+  if (buffers.lock) return 99;
+  buffers.lock = module;
+  return 0;
+}
+
+unsigned char buffers_unlock(unsigned char module)
+{
+  if (buffers.lock!=module) return 98;
+  buffers.lock = LOCK_FREE;
+  return 0;
+}
--- src/telephony/buffers.h
+++ src/telephony/buffers.h
@@ -1,8 +1,13 @@
-#include "includes.h"
+#ifndef BUFFERS_H
+#define BUFFERS_H
+
+#include "records.h"
 
 // We sort 160 records = 80KB = 1/10th of a disk image at a time.
+// (This value MUST be < 0xff, as 0xff is used as record number to
+//  invalidate the rec_b cache via setting b_idx = 0xff; )
 #define QSORT_MAX_RECORDS 160
-#define QSORT_STACK_SIZE  160  // safe headroom
+#define QSORT_STACK_SIZE  160  
 
 typedef struct {
     unsigned char low;
@@ -14,16 +19,34 @@ struct sort_buffers {
   unsigned char indices[QSORT_MAX_RECORDS];  // indices into your slab records
   // Quick sort manual stack
   partition_t stack[QSORT_STACK_SIZE];
-  int stack_ptr = 0;
+  int stack_ptr;
   
   // A and B record buffers for comparison of records during quick sort
   unsigned char rec_a[RECORD_DATA_SIZE];
-  unsigned char rec_b[RECORD_DATA_SIZE];  
+  unsigned char rec_b[RECORD_DATA_SIZE];
+  unsigned char idx_b;
+};
+
+struct index_buffers {
+  unsigned char rec[RECORD_DATA_SIZE];
 };
 
 struct shared_buffers {
+
+#define LOCK_FREE 0x00
+#define LOCK_INDEX 0x01
+#define LOCK_SORT 0x02
+  unsigned char lock;
+  
   union {
     struct sort_buffers sort;
     struct index_buffers index;
   };
 };
+
+extern struct shared_buffers buffers;
+
+unsigned char buffers_lock(unsigned char module);
+unsigned char buffers_unlock(unsigned char module);
+
+#endif
